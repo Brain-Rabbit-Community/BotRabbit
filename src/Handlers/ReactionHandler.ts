ae65461b122e80b3e978d73de056d28fbb726540
Murillo Mendonça <murillo.ventura1711@unifei.edu.br>
2021-04-21 16:11:10 -0300

    Added welcome message

--- src/Handlers/ReactionHandler.ts
+++ src/Handlers/ReactionHandler.ts
@@ -1,5 +1,7 @@
-import { MessageReaction, User, PartialUser } from 'discord.js';
-import { beginnersRoleId, rulesMsgId } from '../configs/config';
+import {
+  MessageReaction, User, PartialUser, Channel, TextChannel,
+} from 'discord.js';
+import { beginnersRoleId, rulesMsgId, welcomeGuildId } from '../configs/config';
 
 export default class ReactionHandler {
   public static async handle(reaction: MessageReaction, user: User | PartialUser) : Promise<void> {
@@ -9,6 +11,8 @@ export default class ReactionHandler {
 
         if (!guildMember?.roles.cache.get(beginnersRoleId)) {
           guildMember?.roles.add(beginnersRoleId);
+          const welcomeChannel = reaction.message.guild?.channels.cache.get(welcomeGuildId);
+          (welcomeChannel as TextChannel).send(`Olá ${user.username}:hand_splayed:, bem vindo(a) ao Deno Brazil, antes de começar leia algumas informações base em #:no_entry_sign:regras e acompanhe sempre as #:mega:novidades .`);
         }
       }
     } catch (Err) {
